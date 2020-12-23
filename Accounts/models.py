4046afaba88594b37cf75d2549f5e078f801d04b
javad jafari <javajafarifromsharak@gmail.com>
2020-12-23 14:36:20 +0330

    models of Account and Product added

---
+++ Accounts/models.py
@@ -0,0 +1,90 @@
+from django.contrib.auth.base_user import BaseUserManager,AbstractBaseUser
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+from django.utils import timezone
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    first_name = models.CharField(_('first name'),max_length=90)
+    last_name = models.CharField(_('last name'),max_length=90)
+    passsword = models.CharField(_('password'), max_length=150)
+    email = models.EmailField(_('email address'),unique=True,db_index=True)
+    mobile = models.CharField(_('mobile'),)
+    avatar = models.CharField(_('avatar'),upload_to='/user/image/')
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"),default=timezone.now)
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['mobile','passsword']
+    objects = UserManager()
+
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+
+    
+
+
+class UserEmail(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    subject = models.CharField(_('subject') )
+    body = models.TextField(_('body'))
+    
+
+class Address(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    city = models.CharField(_('city'),max_length=60)
+    street = models.CharField(_('street'),max_length=160)
+    alley = models.CharField(_('alley'),max_length=160)
+    zip_code = models.CharField(_('zip_code'),max_length=160)
+
+
+class Shop(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(_('name'),max_length=60)
+    slug = models.SlugField(_('slug'),)
+    description = models.CharField(_('description'),max_length=260)
+    image = models.ImageField(verbose_name=_('image'),upload_to='/shop/image/')
---
+++ Products/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import get_user_model
+from Accounts.models import Shop
+# Create your models here.
+
+User = get_user_model()
+
+class Category(models.Model):
+    name = models.CharField(_('name'),max_length=50)
+    slug = models.SlugField(_('slug'))
+    detail = models.TextField(_('detail'))
+    image = models.ImageField(_('image'),upload_to='/category/image/')
+    parent = models.ForeignKey(
+        'self', verbose_name=_("Parent"), on_delete=models.SET_NULL, null=True, blank=True,
+        related_name='children', related_query_name='children')
+    
+
+class Brand(models.Model):
+    name = models.CharField(_('name'),max_length=50)
+    detail = models.TextField(_('detail'))
+    image = models.ImageField(_('image'),upload_to='/brand/image/')
+
+class Product(models.Model):
+    name = models.CharField(_('name'),max_length=50)
+    slug = models.SlugField(_('slug'))
+    detail = models.TextField(_('detail'))
+    image = models.ImageField(_('image'),upload_to='/product/image/')
+    brand = models.ForeignKey(Brand)
+    category = models.ForeignKey(Category, related_name='products', verbose_name=_(
+        "Category"), on_delete=models.SET_NULL, null=True, blank=True)
+
+class ProductMeta(models.Model):
+    product= models.ForeignKey(Product)
+    label = models.CharField(_('label'),max_length=50)
+    value = models.IntegerField(_('value'))
+
+class Image(models.Model):
+    product= models.ForeignKey(Product)
+    image = models.ImageField(_('image'),upload_to='/image/image/')
+
+class Comment(models.Model):
+    content = models.TextField(_("Content"))
+    product = models.ForeignKey(Product, related_name='comments', related_query_name='comments', verbose_name=_(
+        "Product"), on_delete=models.CASCADE)
+    create_at = models.DateTimeField(_("Create at"), auto_now_add=True)
+    update_at = models.DateTimeField(_("Update at"), auto_now=True)
+    author = models.ForeignKey(User, verbose_name=_(
+        "Author"), on_delete=models.CASCADE)
+    is_confirmed = models.BooleanField(_("confirm"), default=True)
+
+    class Meta:
+        verbose_name = _("Comment")
+        verbose_name_plural = _("Comments")
+        ordering = ['-create_at']
+
+class ShopProduct(models.Model):
+    shop = models.ForeignKey(Shop,related_name='ShopProducts', related_query_name='ShopProducts', verbose_name=_(
+        "ShopProduct"), on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='ShopProducts', related_query_name='ShopProducts', verbose_name=_(
+        "ShopProduct"), on_delete=models.CASCADE)
+    price = models.FloatField(_('price'))
+    quantity = models.IntegerField(_('quantity'))
